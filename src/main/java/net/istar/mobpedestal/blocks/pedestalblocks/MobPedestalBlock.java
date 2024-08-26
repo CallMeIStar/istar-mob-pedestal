e97cc336404e375d2a00f267d088cf17bb9c8fec
IStar <stargamemail8@gmail.com>
2024-08-26 17:28:05 +0300

    Still buggy

--- src/main/java/net/istar/mobpedestal/blocks/pedestalblocks/MobPedestalBlock.java
+++ src/main/java/net/istar/mobpedestal/blocks/pedestalblocks/MobPedestalBlock.java
@@ -7,13 +7,21 @@ import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 
 
 public abstract class MobPedestalBlock extends Block implements BlockEntityProvider {
+    public static final BooleanProperty ACTIVE = BooleanProperty.of("active");
+
     private final MobType mobType;
     private final int radius;
 
@@ -21,6 +29,7 @@ public abstract class MobPedestalBlock extends Block implements BlockEntityProvi
         super(settings);
         this.mobType = mobType;
         this.radius = radius;
+        setDefaultState(this.stateManager.getDefaultState().with(ACTIVE, true));
     }
 
     public MobType getMobType() {
@@ -29,6 +38,11 @@ public abstract class MobPedestalBlock extends Block implements BlockEntityProvi
 
     public int getRadius() {
         return radius;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(ACTIVE);
     }
 
     @Override
@@ -47,9 +61,23 @@ public abstract class MobPedestalBlock extends Block implements BlockEntityProvi
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
         super.onPlaced(world, pos, state, placer, itemStack);
-        if (!world.isClient()) {
+        if (!world.isClient() && state.get(ACTIVE)) {
             MobPedestalTracker.addTotem(mobType, pos, radius);
         }
     }
 
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            boolean isActive = state.get(ACTIVE);
+            world.setBlockState(pos, state.with(ACTIVE, !isActive));
+            if (isActive) {
+                MobPedestalTracker.removeTotem(mobType, pos, radius);
+            } else {
+                MobPedestalTracker.addTotem(mobType, pos, radius);
+            }
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
+    }
 }
